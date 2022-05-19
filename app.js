606a021e17e690eb6545200431e804674b0f31ed
Nak <ratanak.rin@stonybrook.edu>
2022-05-19 10:51:14 +0900

    Fully finished implementing EditQuestion part both Backend and Frontend

--- app.js
+++ app.js
@@ -13,5 +13,48 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).t
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+const Question = require('./models/Question');
+
+app.get('/api/questions', async (req, res) => {
+  const questions = await Question.find();
+
+  res.json(questions);
+})
+
+app.post('/api/questions', async (req, res) => {
+  const question = new Question({
+    text: req.body.text,
+    type: req.body.type,
+    date: req.body.date,
+    choices: req.body.choices
+  });
+
+  question.save();
+
+  res.json(question)
+})
+
+app.put('/api/questions/:id', async function (req,res) {
+  let id = req.params.id;
+  Question.findByIdAndUpdate(id,
+      {'text': req.body.text, "type": req.body.type, "date": req.body.date, "choices": req.body.choices},
+      function (err, result) {
+          if (err) {
+              console.log("ERROR: " + err);
+              res.send(err);
+          } else {
+              // Status 204 represents success with no content
+              // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
+              res.sendStatus(204);
+          }
+      });
+});
+
+app.delete('/api/questions/:id', async (req,res) => {
+  const result = await Question.findByIdAndDelete(req.params.id);
+
+  res.json(result);
+});
+
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- models/Question.js
+++ models/Question.js
@@ -1,1 +1,15 @@
-// NOTE test
+// NOTE Question Schema
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema(
+  {
+    text: {type: String},
+    type: {type: String},
+    date: {type: String},
+    choices: {type: Array}
+  }
+)
+
+module.exports = mongoose.model('Question', QuestionSchema);
