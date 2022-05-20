81f6b4203a9e41c1fff008ec41bb61fdb5dddaa7
sjchae <sjchae416@gmail.com>
2022-05-20 20:43:23 +0900

    initial validator.js

--- utils/validator.js
+++ utils/validator.js
@@ -1,1 +1,6 @@
-// NOTE test
+// REVIEW source from prev assignment; check and edit before use
+
+// NOTE Check if it is a valid email address
+// exports.validateEmail = function (email) {
+// 	return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
+// };
