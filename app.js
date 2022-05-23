ce5d499caaf2fb439f5e3c60d998bcf263c01974
sjchae <sjchae416@gmail.com>
2022-05-23 16:14:31 +0900

    refactor app.js

--- app.js
+++ app.js
@@ -1,59 +1,77 @@
 // NOTE setup MongoDB and Express here
 const express = require('express');
 const mongoose = require('mongoose');
+const Question = require('./models/Question');
 const cors = require('cors');
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-var mongoDB = 'mongodb+srv://Cse316rnsj:cse316finalp@cluster0.le5hh.mongodb.net/DayLogger'
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Connected to DB")).catch(console.error);
+var mongoDB =
+	'mongodb+srv://Cse316rnsj:cse316finalp@cluster0.le5hh.mongodb.net/DayLogger';
+mongoose
+	.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('Connected to DB'))
+	.catch(console.error);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const Question = require('./models/Question');
-
+// NOTE get all questions
 app.get('/api/questions', async (req, res) => {
-  const questions = await Question.find();
-
-  res.json(questions);
-})
-
-app.post('/api/questions', async (req, res) => {
-  const question = new Question({
-    text: req.body.text,
-    type: req.body.type,
-    date: req.body.date,
-    choices: req.body.choices
-  });
-
-  question.save();
-
-  res.json(question)
-})
-
-app.put('/api/questions/:id', async function (req,res) {
-  let id = req.params.id;
-  Question.findByIdAndUpdate(id,
-      {'text': req.body.text, "type": req.body.type, "date": req.body.date, "choices": req.body.choices},
-      function (err, result) {
-          if (err) {
-              console.log("ERROR: " + err);
-              res.send(err);
-          } else {
-              // Status 204 represents success with no content
-              // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
-              res.sendStatus(204);
-          }
-      });
+	const questions = await Question.find();
+	res.json(questions);
 });
 
-app.delete('/api/questions/:id', async (req,res) => {
-  const result = await Question.findByIdAndDelete(req.params.id);
+// app.get('/api/questions', async function (req, res) {
+// 	const questions = await Question.find({ agent: req.session.userId });
+// 	const modifiedQuestions = questions.map((mappedQuestion) => {
+// 		return mappedQuestion.toObject();
+// 	});
+// 	res.json(modifiedQuestions.reverse());
+// });
 
-  res.json(result);
+// NOTE create new question
+app.post('/api/questions', async (req, res) => {
+	const newQuestion = new Question({
+		text: req.body.text,
+		type: req.body.type,
+		date: req.body.date,
+		choices: req.body.choices,
+	});
+	await newQuestion.save();
+	res.json(newQuestion);
+});
+
+// NOTE udpdate a question
+app.put('/api/questions/:id', async function (req, res) {
+	let id = req.params.id;
+	Question.findByIdAndUpdate(
+		id,
+		{
+			text: req.body.text,
+			type: req.body.type,
+			date: req.body.date,
+			choices: req.body.choices,
+		},
+		function (err, result) {
+			if (err) {
+				console.log('ERROR: ' + err);
+				res.send(err);
+			} else {
+				// Status 204 represents success with no content
+				// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
+				res.sendStatus(204);
+			}
+		}
+	);
+});
+
+// NOTE delete a question
+app.delete('/api/questions/:id', async (req, res) => {
+	const result = await Question.findByIdAndDelete(req.params.id);
+
+	res.json(result);
 });
 
 const port = 5000;
