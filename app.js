369dd44e02017c6e9960e5209f2bc81404c65cb6
sjchae <sjchae416@gmail.com>
2022-05-30 01:19:51 +0900

    commented cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const User = require('./models/user');
 
 // app.use(express.json());
 // app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
+// app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
