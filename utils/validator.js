877ad3c138f770ead854136789932cfb7d4c2dd0
sjchae <sjchae416@gmail.com>
2022-06-06 14:59:06 +0900

    added password validator

--- utils/validator.js
+++ utils/validator.js
@@ -5,5 +5,5 @@ exports.validateEmail = function (email) {
 
 // NOTE Check if it is a valid password
 exports.validatePassword = function (password) {
-	return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(password);
+	return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/.test(password);
 };
